0f563f8ac6b4d83597a3cbe401ca89ebc82a1e67
Baptiste Jonglez <zorun.42@gmail.com>
2011-06-26 16:34:59 +0200

    Update download page to make it a bit more useful

--- download.php
+++ download.php
@@ -1,14 +1,44 @@
 <?php
 
-$version = "1.3beta5";
-// temporary
-$baseurl = "/zorun/releases";
+function format_bytes($size) {
+  $units = array(' B', ' KiB', ' MiB', ' GiB', ' TiB');
+  for ($i = 0; $size >= 1024 && $i < 4; $i++) $size /= 1024;
+  return round($size, 2).$units[$i];
+}
+
+function get_fullpath($file) {
+  return '../releases/'.$file;
+}
+
+function get_size($file) {
+  return format_bytes(filesize($file));
+}
 
 // prevents being called directly
 if (isset($page) && $page == "download") {
 
+  include('spyc/spyc.php');
+
+  // information about which version are available, along with the date
+  $downloads = Spyc::YAMLLoad('content/download.yaml');
+
+  // we only need the latest version here
+  $version = $downloads[0][0];
+  $date = $downloads[0][1];
+
+  $filenames = array_map("get_fullpath",
+                         array('src' => "$version/asciiportal-$version-src.tar.gz",
+                               'win32' => "$version/asciiportal-$version-win32.zip",
+                               'linux32' => "$version/asciiportal-$version-linux32.tar.xz",
+                               'linux64' => "$version/asciiportal-$version-linux64.tar.xz"));
+
+  // check the sizes of the files
+  $sizes = array_map("get_size", $filenames);
+
   $smarty->assign("version", $version);
-  $smarty->assign("baseurl", $baseurl);
+  $smarty->assign("release_date", $date);
+  $smarty->assign("filenames", $filenames);
+  $smarty->assign("sizes", $sizes);
   $smarty->display("download.tpl");
   
 }
--- templates/download.tpl
+++ templates/download.tpl
@@ -1,10 +1,16 @@
 
 <h1>Download ASCIIpOrtal</h1>
 
-<p>Latest version: <strong>{$version}</strong></p>
+<p>Latest version: <strong>{$version}</strong> ({$release_date})</p>
 
 {* Windows version *}
 <img src="static/windows.png" alt="Windows version" />
 
 {* Linux version *}
 <img src="static/linux.png" alt="Linux version" />
+
+<ul>
+{foreach $filenames as $key=>$filename}
+  <li><a href="{$filename}">{$key}:</a> ({$sizes[$key]})</li>
+{/foreach}
+</ul>
